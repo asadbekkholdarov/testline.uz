d0a83b62c3d01a1b79a3db789784e165f9d5301a
asadbekkholdarov <507asadali@gmail.com>
2025-08-21 14:42:54 +0500

    small changes at footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -105,7 +105,7 @@ export default function Footer() {
 
         <div className="border-t border-gray-800 mt-12 pt-8 text-center">
           <p className="text-gray-400">
-            © {new Date().getFullYear()} Testline. {t('footerCopyright')}.
+            © {new Date().getFullYear()} Testline!. {t('footerCopyright')}.
           </p>
         </div>
       </div>
